2b2c7da38697e2286db842ed1bf13ccab904ac7a
Ethan Smith <esmith@ethanskills.com>
2024-08-02 10:22:34 -0400

    Center map at Gainesville + Teest Locations

---
+++ src/components/ScooterMap.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+interface position {
+  lat: number
+  lng: number
+}
+const props = defineProps<{
+  center: position
+  markers: { position: position }[]
+}>()
+</script>
+<template>
+  <GMapMap :center="center"
+    :zoom="16"
+    map-type-id="terrain"
+    style="width: 100vw; height: 20rem"
+    :options="{
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      streetViewControl: true,
+      rotateControl: true,
+      fullscreenControl: true
+    }">
+    <GMapMarker :key="index"
+      v-for="(m, index) in props.markers"
+      :position="m.position"
+      :clickable="true">
+      <GMapInfoWindow>
+        <div class="location">
+          Scooter
+        </div>
+      </GMapInfoWindow>
+    </GMapMarker>
+
+  </GMapMap>
+
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,26 @@
-<script setup>
-import { onMounted, ref } from 'vue'
-const center = { lat: 51.093048, lng: 6.842120 }
-const markers = [
+<script setup lang="ts">
+import ScooterMap from 'src/components/ScooterMap.vue';
+const mapCenter = {
+  lat: 29.6465,
+  lng: -82.3533
+}
+const scooterLocations = [
   {
     position: {
-      lat: 51.093048, lng: 6.842120
+      lat: 29.648067984286616,
+      lng: -82.34353394501409
     }
-  }
+  },
+  {
+    position: {
+      lat: 29.648545849384718,
+      lng: -82.34352858059584
+    }
+  },
 ]
 </script>
 <template>
-  <GMapMap :center="center"
-    :zoom="10"
-    map-type-id="terrain"
-    style="width: 100vw; height: 20rem"
-    :options="{
-      zoomControl: true,
-      mapTypeControl: true,
-      scaleControl: true,
-      streetViewControl: true,
-      rotateControl: true,
-      fullscreenControl: true
-    }">
-    <GMapMarker :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      @click="center = m.position">
-      <GMapInfoWindow>
-        <div class="location">
-          Scooter
-        </div>
-      </GMapInfoWindow>
-    </GMapMarker>
-
-  </GMapMap>
-
+  29.648545849384718, -82.34352858059584
+  <ScooterMap :markers="scooterLocations"
+    :center="mapCenter" />
 </template>
