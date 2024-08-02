2c2a47d9c6128cdb0b2ece127c2627bd39987f54
Ethan Smith <esmith@ethanskills.com>
2024-08-02 09:32:20 -0400

    Rename login route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
       { path: 'register', component: () => import('pages/RegisterPage.vue') },
     ],
   },
