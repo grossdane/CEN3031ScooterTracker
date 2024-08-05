8de589daca640df2b005d6e9968363779b17f682
Ethan Smith <esmith@ethanskills.com>
2024-08-05 09:10:46 -0400

    Configure supabase composable

--- src/components/Admin/AdminScooters.vue
+++ src/components/Admin/AdminScooters.vue
@@ -1,5 +1,16 @@
+<script lang="ts" setup>
+import { useSupabase } from 'src/composables/use-supabase';
+const { supabase } = useSupabase();
+
+let { data: scooters, error } = await supabase
+  .from('scooters')
+  .select('*')
+
+</script>
 <template>
   <div>
-    <div class="tw-text-xl">All Scooters</div>
+    <div class="tw-text-xl tw-font-medium">All Scooters
+      {{ scooters }}
+    </div>
   </div>
 </template>
--- src/pages/Admin/IndexPage.vue
+++ src/pages/Admin/IndexPage.vue
@@ -1,13 +1,12 @@
 <script lang="ts" setup>
 import AdminHeader from 'src/components/Admin/AdminHeader.vue';
+import AdminScooters from 'src/components/Admin/AdminScooters.vue';
 </script>
 <template>
   <AdminHeader />
   <div class="tw-container tw-mx-auto">
-    <div class="tw-grid tw-grid-cols-3">
-      <div>
-        <div>Scooter Requests</div>
-      </div>
+    <div class="tw-grid tw-grid-cols-2">
+      <AdminScooters />
     </div>
   </div>
 </template>
