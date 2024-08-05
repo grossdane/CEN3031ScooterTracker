892bd3c440e6e646f1ba3aba3541f1808d9be089
Ethan Smith <esmith@ethanskills.com>
2024-08-05 09:21:55 -0400

    Install Ag-grid table

--- src/components/Admin/AdminScooters.vue
+++ src/components/Admin/AdminScooters.vue
@@ -1,8 +1,25 @@
 <script lang="ts" setup>
-import { useSupabase } from 'src/composables/use-supabase';
+import { ref } from 'vue';
 import { useScooterQuery } from 'src/composables/use-scooter-query';
-const { supabase } = useSupabase();
-const { data, isLoading } = useScooterQuery();
+const { data } = useScooterQuery();
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import { AgGridVue } from 'ag-grid-vue3';
+
+const rowData = ref([
+  { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+  { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+  { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+]);
+
+// Column Definitions: Defines the columns to be displayed.
+const colDefs = ref([
+  { field: 'make' },
+  { field: 'model' },
+  { field: 'price' },
+  { field: 'electric' }
+]);
 
 
 </script>
@@ -10,7 +27,11 @@ const { data, isLoading } = useScooterQuery();
   <div>
     <div class="tw-text-xl tw-font-medium">All Scooters
       {{ data }}
-      {{ isLoading }}
+      <ag-grid-vue :rowData="rowData"
+        :columnDefs="colDefs"
+        style="height: 500px"
+        class="ag-theme-quartz">
+      </ag-grid-vue>
     </div>
   </div>
 </template>
