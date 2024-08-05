6b51957cd91dcb2f9d7a3bd2481e12f5d99f5ce0
Ethan Smith <esmith@ethanskills.com>
2024-08-05 19:20:42 -0400

    Implement login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,22 +1,38 @@
 <script lang="ts" setup>
 import { ref, Ref } from 'vue'
 import { useSupabase } from 'src/composables/use-supabase';
+import { useRouter } from 'vue-router';
+import { useQuasar } from 'quasar';
+const $q = useQuasar();
 const { supabase } = useSupabase();
+const router = useRouter();
 const email: Ref<string> = ref('');
 const password: Ref<string> = ref('');
 const handleUserLogin = async () => {
-  const user = await supabase.auth.getUser()
-  console.log(user)
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  });
+  if (error) {
+    console.error('Error logging in:', error.message);
+    $q.notify({
+      type: 'negative',
+      message: error.message
+    });
+  }
+  if (data) {
+    router.push('/');
+  }
 }
 </script>
 
 <template>
-  <q-page class="flex flex-center">
-    <q-card style="width: 400px">
+  <q-page class="tw-flex tw-justify-center tw-items-center">
+    <q-card class="tw-w-[400px] tw-flex tw-flex-col tw-h-[380px]">
       <q-card-section class="row items-center q-pb-none">
         <div>
           <div class="text-h6">Scoot3r</div>
-          <div class="text-subtitle2">Please login to your account</div>
+          <div class="text-subtitle2 tw-mb-4">Please login to your account</div>
         </div>
       </q-card-section>
 
@@ -25,9 +41,9 @@ const handleUserLogin = async () => {
       <q-card-section>
         <q-form @submit="handleUserLogin">
           <q-input v-model="email"
-            label="Username"
+            label="Email"
             filled
-            :rules="[val => !!val || 'Username is required']"
+            :rules="[val => !!val || 'Email is required']"
             class="q-mb-md" />
 
           <q-input v-model="password"
@@ -40,14 +56,19 @@ const handleUserLogin = async () => {
           <q-btn type="submit"
             label="Login"
             color="primary"
-            class="full-width" />
+            no-caps
+            class="full-width tw-mb-4" />
+          <q-btn to="/register"
+            unelevated
+            no-caps
+            class="q-text-center q-mx-auto full-width">Create Account</q-btn>
         </q-form>
       </q-card-section>
-      <div class="q-flex">
-        <q-btn to="/register"
-          class="q-text-center q-mx-auto full-width">Register</q-btn>
-      </div>
+
     </q-card>
+    <div class="q-flex">
+
+    </div>
   </q-page>
 </template>
 
