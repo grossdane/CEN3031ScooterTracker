2dc394807bbe830460dd73939ae1ca832c26e873
Ethan Smith <esmith@ethanskills.com>
2024-08-05 20:46:05 -0400

    Implement action tooltips + other actions

--- src/components/Admin/AdminActionCell.vue
+++ src/components/Admin/AdminActionCell.vue
@@ -3,7 +3,7 @@ import { useUpdateRentalMutation, useGetRentalsQuery } from 'src/composables/use
 const { mutate: updateRental } = useUpdateRentalMutation();
 const { refetch } = useGetRentalsQuery();
 const props = defineProps<{
-  params: Record<string, string>;
+  params: Record<string, any>;
 }>();
 console.log(props.params.value)
 const handleUpdateScooter = async (id: string, status: string) => {
@@ -12,12 +12,25 @@ const handleUpdateScooter = async (id: string, status: string) => {
 }
 </script>
 <template>
-  <div>
+  <div class="tw-text-center">
     <q-icon name="check"
+      v-if="props.params.node.data.status === 'Pending'"
       class="tw-text-green-600 tw-cursor-pointer tw-font-bold tw-text-xl tw-mr-2"
-      @click="handleUpdateScooter(props.params.value, 'Approved')" />
+      @click="handleUpdateScooter(props.params.value, 'Approved')">
+      <q-tooltip>Approve Scooter Request</q-tooltip>
+
+    </q-icon>
     <q-icon name="close"
+      v-if="props.params.node.data.status === 'Pending'"
       class="tw-text-red-600 tw-cursor-pointer tw-font-bold tw-text-xl"
-      @click="handleUpdateScooter(props.params.value, 'Rejected')" />
+      @click="handleUpdateScooter(props.params.value, 'Rejected')">
+      <q-tooltip>Reject Scooter Request</q-tooltip>
+    </q-icon>
+    <q-icon name="pause"
+      v-if="props.params.node.data.status === 'Approved'"
+      class="tw-text-yellow-600 tw-cursor-pointer tw-font-bold tw-text-xl"
+      @click="handleUpdateScooter(props.params.value, 'Returned')">
+      <q-tooltip>Mark scooter as Returned</q-tooltip>
+    </q-icon>
   </div>
 </template>
